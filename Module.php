6c466d5a676dde782d0749013b5a938fe24d844a
Max <maks757q@gmail.com>
2016-03-18 13:02:14 +0200

    V 0.0.3-dev module translations from file

--- Module.php
+++ Module.php
@@ -38,4 +38,16 @@ class Module extends \yii\base\Module
             }
         }
     }
+
+    public function registerTranslations()
+    {
+        \Yii::$app->i18n->translations['modules/users/*'] = [
+            'class'          => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath'       => '@vendor/black-lamp/articles/lang',
+            'fileMap'        => [
+            ],
+        ];
+    }
+
 }
