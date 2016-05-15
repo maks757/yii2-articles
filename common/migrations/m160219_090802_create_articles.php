0fd0be0e9a42d327c50b49bf5b5e3bc5482a444e
Вадим <guts.vadim@gmail.com>
2016-05-15 07:36:20 +0300

    check if table exists before install

--- common/migrations/m160219_090802_create_articles.php
+++ common/migrations/m160219_090802_create_articles.php
@@ -6,41 +6,43 @@ class m160219_090802_create_articles extends Migration
 {
     public function up()
     {
-        $this->createTable('article_category',
-            [
-                'id' => $this->primaryKey(),
-                'parent_id' => $this->integer(),
-            ]);
-        $this->addForeignKey('article_category_fk', 'article_category', 'parent_id', 'article_category', 'id', 'CASCADE', 'CASCADE');
+        if(!$this->getDb()->getTableSchema('article')) {
+            $this->createTable('article_category',
+                [
+                    'id' => $this->primaryKey(),
+                    'parent_id' => $this->integer(),
+                ]);
+            $this->addForeignKey('article_category_fk', 'article_category', 'parent_id', 'article_category', 'id', 'CASCADE', 'CASCADE');
 
-        $this->createTable('article',
-            [
-                'id' => $this->primaryKey(),
-                'category_id' => $this->integer(),
-            ]);
-        $this->addForeignKey('article_category_article_fk', 'article', 'category_id', 'article_category', 'id', 'CASCADE', 'CASCADE');
+            $this->createTable('article',
+                [
+                    'id' => $this->primaryKey(),
+                    'category_id' => $this->integer(),
+                ]);
+            $this->addForeignKey('article_category_article_fk', 'article', 'category_id', 'article_category', 'id', 'CASCADE', 'CASCADE');
 
-        $this->createTable('article_category_translation',
+            $this->createTable('article_category_translation',
+                [
+                    'id' => $this->primaryKey(),
+                    'language_id' => $this->integer(),
+                    'category_id' => $this->integer(),
+                    'name' => $this->string(),
+                    'text' => $this->text(),
+                    'short_text' => $this->text(),
+                ]);
+            $this->addForeignKey('article_category_translation_category_fk', 'article_category_translation', 'category_id', 'article_category', 'id', 'CASCADE', 'CASCADE');
+
+            $this->createTable('article_translation',
             [
                 'id' => $this->primaryKey(),
                 'language_id' => $this->integer(),
-                'category_id' => $this->integer(),
+                'article_id' => $this->integer(),
                 'name' => $this->string(),
                 'text' => $this->text(),
                 'short_text' => $this->text(),
             ]);
-        $this->addForeignKey('article_category_translation_category_fk', 'article_category_translation', 'category_id', 'article_category', 'id', 'CASCADE', 'CASCADE');
-
-        $this->createTable('article_translation',
-        [
-            'id' => $this->primaryKey(),
-            'language_id' => $this->integer(),
-            'article_id' => $this->integer(),
-            'name' => $this->string(),
-            'text' => $this->text(),
-            'short_text' => $this->text(),
-        ]);
-        $this->addForeignKey('article_translation_article_fk', 'article_translation', 'article_id', 'article', 'id', 'CASCADE', 'CASCADE');
+            $this->addForeignKey('article_translation_article_fk', 'article_translation', 'article_id', 'article', 'id', 'CASCADE', 'CASCADE');
+        }
 
     }
 
